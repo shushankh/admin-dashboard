4f51482d58bb46a048db80eacdc94935d58ba05e
shushankh <basnetshushankh@gmail.com>
2026-07-29 14:47:00 +0545

    fix: correct typed Redux Dispactch

--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -3,4 +3,4 @@ import authReducer from "./auth/authSlice";
 export const store = configureStore({ reducer: { auth: authReducer } });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = ReturnType<typeof store.dispatch>;
+export type AppDispatch = typeof store.dispatch;
